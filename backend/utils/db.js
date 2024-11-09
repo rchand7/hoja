d3ec0916a92b015e9bc02e63b0c8f8ee0955acf4
Rohit Chand <chandrohitsumit@gmail.com>
2024-11-09 14:59:23 +0530

    Initial commit for hoja project

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
+        console.log('MongoDB URI:', process.env.MONGO_URI); // Add this log
         await mongoose.connect(process.env.MONGO_URI);
         console.log('mongodb connected successfully');
     } catch (error) {
-        console.log(error);
+        console.log('Error connecting to MongoDB:', error);
     }
-}
+};
+
 export default connectDB;
